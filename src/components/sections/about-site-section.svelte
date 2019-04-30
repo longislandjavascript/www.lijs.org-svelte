523e9883ace3bb16ad42e9711cddfde83ec9d2e4
gojutin <gojutin@gmail.com>
2019-04-30 14:27:10 -0400

    Fixed broken import.

--- src/components/sections/about-site-section.svelte
+++ src/components/sections/about-site-section.svelte
@@ -8,7 +8,7 @@
 </div>
 
 <script lang="ts">
-	import  ImageLink  from "../common/ImageLink";
+	import  ImageLink  from "../common/ImageLink.svelte";
 	import { CLOUDINARY_BASE_URL } from "../../env_vars";
 	const BASE_URL: string = `${CLOUDINARY_BASE_URL}/w_300/v1556492206/lijs.org`;
 
