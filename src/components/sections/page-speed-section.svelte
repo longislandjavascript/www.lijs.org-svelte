6ec30704c84e92e3f6b5f324aeeb5e50b93897f0
gojutin <gojutin@gmail.com>
2019-04-30 16:12:27 -0400

    Updated speed test results UI.

--- src/components/sections/page-speed-section.svelte
+++ src/components/sections/page-speed-section.svelte
@@ -1,44 +1,48 @@
 <div class="container">
   {#await speedIndex}
-  <h3>A PageSpeed Insights test in progress...</h3>
-  <h3>The results will show up below once the test is complete.</h3>
+  <div class="pending-wrapper">
+    <h3>A PageSpeed Insights test in progress</h3>
+    <Spinner size="30" speed="750" color="gold" thickness="2" gap="20" />
+  </div>
+
+  <h5>The results will show up here once the test is complete.</h5>
 
   {:then data}
   <div transition:fade>
     <h3>Results from PageSpeed Insights test</h3>
+    <h5>Completed on {format(data.analysisUTCTimestamp, 'MMMM DD, YYYY')} at {format(data.analysisUTCTimestamp, 'hh:MM A')}</h5>
     <div class="badge-wrapper">
       <Badge title="Speed Index"
-      value={data.lighthouseResult.audits["speed-index"].displayValue} /> <Badge
+      value={data.lighthouseResult.audits["speed-index"].displayValue} /> 
+      <Badge
       title="First Paint"
       value={data.lighthouseResult.audits["first-meaningful-paint"].displayValue}
-      /> <Badge title="Time to Interactive"
+      /> 
+      <Badge title="Time to Interactive"
       value={data.lighthouseResult.audits["interactive"].displayValue} />
     </div>
   </div>
+  {:catch error}
+  <h2>Oops...looks like the test didn't work.</h2>
+  <button on:click={() => getSpeedIndex()}>Try again</button>
   {/await}
 </div>
 
 <script>
   import { fade } from "svelte/transition";
   import { Badge } from "../common";
-  const getSpeedIndex = () => {
+  import Spinner from 'svelte-spinner';
+  import { format } from 'date-fns';
+  const getPageSpeedTestData = () => {
     return fetch("/.netlify/functions/fetchPageSpeedTest").then(res => {
       return res.json().then(data => {
+        console.log("DATA", data)
         return data;
       });
     });
   };
 
-  let speedIndex = getSpeedIndex();
-
-  const getWebpageTest = () => {
-    return fetch("/.netlify/functions/fetchWebPageTest").then(res => {
-      return res.json().then(data => {
-        console.log("THE DATA", data);
-        return data;
-      });
-    });
-  };
+  let speedIndex = getPageSpeedTestData();
 </script>
 
 <style>
@@ -53,4 +57,11 @@
     flex-wrap: wrap;
     justify-content: center;
   }
+
+  .pending-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
 </style>
--- src/env_vars.js
+++ src/env_vars.js
@@ -1,2 +1,5 @@
 export const CLOUDINARY_BASE_URL = "<% CLOUDINARY_BASE_URL %>";
 export const PUBLIC_SLACK_URL = "<% PUBLIC_SLACK_URL %>";
+
+// This one is not currently being used.
+export const WEBPAGETEST_PING_URL = "<% WEBPAGETEST_PING_URL  %>";
